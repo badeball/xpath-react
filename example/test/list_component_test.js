7cf944f48f464fa35cde86e8e0efccb9c16ddaa4
Jonas Amundsen <jonasba@gmail.com>
2015-10-01 10:40:13 +0200

    Update the example to utilize the new utils

--- example/test/list_component_test.js
+++ example/test/list_component_test.js
@@ -4,7 +4,7 @@
 
 var React = require("react");
 
-var Helper = require("./helper");
+var XPathUtils = require("xpath-react/utils");
 
 /* eslint-disable no-unused-vars */
 var List = require("../lib/list_component");
@@ -23,7 +23,7 @@ describe("ListComponent", function () {
     it("should render the given items", function () {
       var items = ["foo", "bar"];
 
-      var element = Helper.render(<List isLoading={false} items={items} />);
+      var element = XPathUtils.render(<List isLoading={false} items={items} />);
 
       element.should.have.xpath(".//li[contains(., 'foo')]");
       element.should.have.xpath(".//li[contains(., 'bar')]");
@@ -32,7 +32,7 @@ describe("ListComponent", function () {
     it("should render a delete button for each item", function () {
       var items = ["foo", "bar"];
 
-      var element = Helper.render(<List isLoading={false} items={items} />);
+      var element = XPathUtils.render(<List isLoading={false} items={items} />);
 
       element.should.have.xpath(".//li[contains(., 'foo')]/button[contains(., 'Delete')]");
       element.should.have.xpath(".//li[contains(., 'bar')]/button[contains(., 'Delete')]");
@@ -43,25 +43,25 @@ describe("ListComponent", function () {
 
       var onRemove = this.sinon.spy();
 
-      var element = Helper.render(<List isLoading={false} items={items} onRemove={onRemove} />);
+      var element = XPathUtils.render(<List isLoading={false} items={items} onRemove={onRemove} />);
 
-      var deleteButton = Helper.find(element, ".//button[contains(., 'Delete')]");
+      var deleteButton = XPathUtils.find(element, ".//button[contains(., 'Delete')]");
 
-      Helper.Simulate.click(deleteButton);
+      XPathUtils.Simulate.click(deleteButton);
 
       onRemove.should.have.been.calledWith(0);
     });
 
     it("should render the given form value", function () {
-      var element = Helper.render(<List isLoading={false} formValue="foo" />);
+      var element = XPathUtils.render(<List isLoading={false} formValue="foo" />);
 
-      var textarea = Helper.find(element, ".//textarea");
+      var textarea = XPathUtils.find(element, ".//textarea");
 
       textarea.props.value.should.equal("foo");
     });
 
     it("should render an add item button", function () {
-      var element = Helper.render(<List isLoading={false} />);
+      var element = XPathUtils.render(<List isLoading={false} />);
 
       element.should.have.xpath(".//button[contains(., 'Add')]");
     });
@@ -69,11 +69,11 @@ describe("ListComponent", function () {
     it("should invoke a callback upon pressing the add button", function () {
       var onAdd = this.sinon.spy();
 
-      var element = Helper.render(<List isLoading={false} onAdd={onAdd} formValue="foo" />);
+      var element = XPathUtils.render(<List isLoading={false} onAdd={onAdd} formValue="foo" />);
 
-      var addButton = Helper.find(element, ".//button[contains(., 'Add')]");
+      var addButton = XPathUtils.find(element, ".//button[contains(., 'Add')]");
 
-      Helper.Simulate.click(addButton);
+      XPathUtils.Simulate.click(addButton);
 
       onAdd.should.have.been.calledWith("foo");
     });
