6840bfe052860b4231250ae685eb7378cd4c7f96
Jonas Amundsen <jonasba@gmail.com>
2019-02-23 21:06:03 +0100

    Add some tests for boolean logic
    The observant reader will notice that none of these pass either.

---
+++ test/boolean_test.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var React = require("react");
+
+var Helper = require("./helper");
+
+var document = <h1 />;
+
+var assertEvaluatesToValue = Helper.assertEvaluatesToValue.bind(null, document);
+
+suite("XPathDOM", function () {
+  suite("boolean", function () {
+    test("00", function () {
+      assertEvaluatesToValue("//h1 and //h1", true);
+    });
+
+    test("01", function () {
+      assertEvaluatesToValue("//h1 and //foo", false);
+    });
+
+    test("02", function () {
+      assertEvaluatesToValue("//h1 or //h1", true);
+    });
+
+    test("03", function () {
+      assertEvaluatesToValue("//h1 or //foo", true);
+    });
+
+    test("04", function () {
+      assertEvaluatesToValue("//foo or //h1", true);
+    });
+
+    test("05", function () {
+      assertEvaluatesToValue("//foo or //bar", false);
+    });
+  });
+});
